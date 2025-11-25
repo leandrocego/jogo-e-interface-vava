a20ce9fa8aa624a9bbdfc842988448f3e2044210
leandrocego <leandro.w@escolar.ifrn.edu.br>
2025-11-25 11:59:01 -0300

    jogo quase completo

---
+++ My project/Assets/Script/inimigo.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+
+public class Inimigo : Personagem
+{
+    [SerializeField] private int dano = 1;
+    
+    public float raioDeVisao = 1;
+    public CircleCollider2D _visaoCollider2D;
+
+    [SerializeField] private Transform posicaoDoPlayer;
+    
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    
+    public AudioSource audioSource;
+
+    private bool andando = false;
+    
+    private GerenciadorDeVitoria gerenciadorDeVitoria;
+    
+    public void setDano(int dano)
+    {
+        this.dano = dano;
+    }
+    public int getDano()
+    {
+        return this.dano;
+    }
+    
+    
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        
+        if (posicaoDoPlayer == null)
+        {
+            posicaoDoPlayer =  GameObject.Find("Player").transform;
+           // posicaoDoPlayer =  GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        }
+
+        //gerenciadorDeVitoria = GameObject.Find("Placar").GetComponent<GerenciadorDeVitoria>();
+        gerenciadorDeVitoria = GameObject.FindWithTag("Placar").GetComponent<GerenciadorDeVitoria>();
+        raioDeVisao = _visaoCollider2D.radius;
+        
+        audioSource = GetComponent<AudioSource>();
+
+    }
+    void Update()
+    {
+        andando = false;
+        
+      //  gameObject.transform.LookAt(posicaoDoPlayer, Vector3.up);
+      
+      
+      // Calculate direction from this object to the target
+      Vector3 direction = posicaoDoPlayer.position - transform.position;
+
+      // Calculate angle in degrees (atan2 handles all quadrants)
+      float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+      // Apply rotation around Z-axis only (2D rotation)
+      transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        
+        
+        if (posicaoDoPlayer != null && 
+            Vector3.Distance(posicaoDoPlayer.position, transform.position) <= raioDeVisao )
+        {
+            Debug.Log("Posição do Pluer"+ posicaoDoPlayer.position);
+            
+            transform.position = Vector3.MoveTowards(transform.position, 
+                posicaoDoPlayer.transform.position,
+                getVelocidade() * Time.deltaTime);
+            
+            andando = true;
+        }
+        
+        if (getVida() <= 0)
+        {
+            gerenciadorDeVitoria.killsAtuais += 1;
+            //desativa o objeto do Inimigo
+            gameObject.SetActive(false);
+          
+        }
+        
+       // animator.SetBool("Andando",andando);
+
+    }
+
+    public void playAudio()
+    {
+        audioSource.Play();
+    }
+    
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Causa dano ao Player
+            int novaVida = collision.gameObject.GetComponent<Personagem>().getVida() - getDano();
+            collision.gameObject.GetComponent<Personagem>().setVida(novaVida);
+
+            //collision.gameObject.GetComponent<Personagem>().recebeDano(getDano());
+            
+            //desativa quando bate no player
+            
+            setVida(0);
+            
+            gerenciadorDeVitoria.killsAtuais += 1;
+            gameObject.SetActive(false);
+        }
+    }
+    
+
+}
---
+++ My project/Assets/Script/player.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Player : Personagem
+{
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    
+    public Transform arma;
+
+    private bool amdando;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        amdando = false;
+        
+        
+
+        //movimento para a esquerda
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0, 0);  
+            amdando = true;
+        }
+
+        //movimento para a direita
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
+            amdando = true;
+        }
+        
+        //movimento para a Cima
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0); 
+            amdando = true;
+        }
+        
+        //movimento para a Cima
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0);  
+            amdando = true;
+        }
+        
+        
+        animator.SetBool("Andando", amdando);
+    }
+    
+    
+}
